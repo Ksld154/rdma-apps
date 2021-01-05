30f3e2a7044ec10f5f5c7b10f5d8cbd815e9f1d8
lylin1997 <lylin1997.cs09g@nctu.edu.tw>
2021-01-05 18:44:44 +0800

    [RDMA-4] Add usleep() to workaround with part2 server segmentation fault

--- 02_read-write/rdma-common.h
+++ 02_read-write/rdma-common.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <inttypes.h>
 #include <rdma/rdma_cma.h>
 
 #define TEST_NZ(x) do { if ( (x)) die("error: " #x " failed (returned non-zero)." ); } while (0)
