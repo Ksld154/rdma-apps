ad30be3e4b6b653b871f958676352672aa9a5885
lylin1997 <lylin1997.cs09g@nctu.edu.tw>
2021-09-14 14:24:16 +0800

    [RDMA-1] Adding dummy app on server side

---
+++ [RDMA-1] Dummy_RDMA_APP/dummy_app.h
@@ -0,0 +1,9 @@
+#ifndef RDMA_DUMMY_APP_H
+#define RDMA_DUMMY_APP_H
+
+#include <cstdio>
+#include <cstdlib>
+
+int add_one(char* buf);
+
+#endif
---
+++ [RDMA-1] Dummy_RDMA_APP/messages.h
@@ -0,0 +1,28 @@
+#ifndef RDMA_MESSAGES_H
+#define RDMA_MESSAGES_H
+
+#define BUFFER_SIZE 10240
+
+const char *DEFAULT_PORT = "12345";
+const int REQUEST_LIMIT = 200;
+const int SAME_MESSAGE_PROBABILITY_INVERSE = 2;
+
+enum message_id {
+	MSG_INVALID = 0,
+	MSG_MR,
+	MSG_READY,
+	MSG_DONE
+};
+
+struct message {
+	int id;
+
+	union {
+		struct {
+			uint64_t addr;
+			uint32_t rkey;
+		} mr;
+	} data;
+};
+
+#endif
