c909f80e53bdbe93d2db43d5b1b683147fe537c9
lylin1997 <lylin1997.cs09g@nctu.edu.tw>
2021-09-15 20:20:11 +0800

    [RDMA-1] Client side almost done with a bug
    Client will receive an wc error message when doing disconnect

--- [RDMA-1] Dummy_RDMA_APP/messages.h
+++ [RDMA-1] Dummy_RDMA_APP/messages.h
@@ -4,25 +4,25 @@
 #define BUFFER_SIZE 10240
 
 const char *DEFAULT_PORT = "12345";
-const int REQUEST_LIMIT = 200;
-const int SAME_MESSAGE_PROBABILITY_INVERSE = 2;
+const int   REQUEST_LIMIT = 5;
+const int   SAME_MESSAGE_PROBABILITY_INVERSE = 2;
 
 enum message_id {
-	MSG_INVALID = 0,
-	MSG_MR,
-	MSG_READY,
-	MSG_DONE
+    MSG_INVALID = 0,
+    MSG_MR,
+    MSG_READY,
+    MSG_DONE
 };
 
 struct message {
-	int id;
+    int id;
 
-	union {
-		struct {
-			uint64_t addr;
-			uint32_t rkey;
-		} mr;
-	} data;
+    union {
+        struct {
+            uint64_t addr;
+            uint32_t rkey;
+        } mr;
+    } data;
 };
 
 #endif
